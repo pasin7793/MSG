65b3d731cb4faf62051cae91a2fd490828f32a07
pasin7793 <pasin7793@gmail.com>
2022-05-14 01:13:13 +0900

    Initial Commit

---
+++ MSG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MSG
+//
+//  Created by 임준화 on 2022/05/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
